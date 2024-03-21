943be81e1d94116d04e51a4907f58f514115eeb6
Ravi Asthana <raviast1719@outlook.com>
2024-03-21 14:15:19 +0530

    feat: resolved auth issues | init firebase

--- components/NewChat/index.tsx
+++ components/NewChat/index.tsx
@@ -1,10 +1,23 @@
 import { PlusIcon } from '@heroicons/react/24/solid';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 const NewChat = () => {
+  const router = useRouter();
+  const { data: sessionData } = useSession();
+  
+  const initialiseNewChat = async () => {
+
+  };
+  
   return (
-    <div className="border-gray-700 border button-row">
-      <PlusIcon className="h-4 w-4" />
+    <div
+      role="button"
+      onClick={initialiseNewChat}
+      className="border-gray-700 border button-row"
+    >
+      <PlusIcon className="h-4 w-4 mr-2" />
       New Chat
     </div>
   );
--- components/SideBar/index.tsx
+++ components/SideBar/index.tsx
@@ -1,7 +1,12 @@
+'use client'
+
 import React from 'react';
 import NewChat from '../NewChat';
+import { signOut, useSession } from 'next-auth/react';
 
 const SideBar = () => {
+  const { data: sessionData } = useSession();
+  
   return (
     <div className="p-2 flex flex-col h-screen">
       <div className="flex-1">
@@ -14,6 +19,17 @@ const SideBar = () => {
           {/* Map for ChatRows */}
         </div>
       </div>
+
+      {
+        sessionData && (
+          <img
+            onClick={() => signOut()}
+            src={sessionData.user?.image!}
+            alt="Profile pic"
+            className="h-12 w-12 rounded-full cursor-pointer mx-auto mb-2 hover:opacity-50"
+          />
+        )
+      }
     </div>
   );
 };
